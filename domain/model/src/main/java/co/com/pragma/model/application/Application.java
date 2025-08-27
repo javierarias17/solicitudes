1bf92e9abd607222104699145f74ea696e7245dd
Javier Arias <apedro@unicauca.edu.co>
2025-08-26 19:39:15 -0500

    Se crean pruebas unitarias

--- domain/model/src/main/java/co/com/pragma/model/application/Application.java
+++ domain/model/src/main/java/co/com/pragma/model/application/Application.java
@@ -7,7 +7,7 @@ import java.math.BigDecimal;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
+@Builder(toBuilder = true)
 public class Application {
 
     private Long id;
---
+++ domain/usecase/src/test/java/co/com/pragma/usecase/RegisterLoanApplicationUseCaseTest.java
@@ -0,0 +1,121 @@
+package co.com.pragma.usecase;
+
+import co.com.pragma.model.application.Application;
+import co.com.pragma.model.application.gateways.ApplicationRepository;
+import co.com.pragma.model.loantype.gateways.LoanTypeRepository;
+import co.com.pragma.model.status.gateways.StatusRepository;
+import co.com.pragma.usecase.exceptions.BusinessException;
+import co.com.pragma.usecase.registerloanapplication.RegisterLoanApplicationUseCase;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import java.math.BigDecimal;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class RegisterLoanApplicationUseCaseTest {
+
+    private static final Long VALID_LOAN_TYPE_ID = 10L;
+    private static final Long INVALID_LOAN_TYPE_ID = 21L;
+    private static final Long PENDING_STATUS_ID = 1L;
+    private static final BigDecimal APPLICATION_AMOUNT = BigDecimal.valueOf(1000.00);
+
+    @InjectMocks
+    private RegisterLoanApplicationUseCase registerLoanApplicationUseCase;
+
+    @Mock
+    private ApplicationRepository applicationRepository;
+    @Mock
+    private StatusRepository statusRepository;
+    @Mock
+    private LoanTypeRepository loanTypeRepository;
+
+    @Test
+    void shouldCreateLoanApplicationWhenDataIsValid() {
+        Application application = Application.builder()
+                .id(null)
+                .amount(APPLICATION_AMOUNT)
+                .loanTypeId(VALID_LOAN_TYPE_ID)
+                .statusId(PENDING_STATUS_ID)
+                .build();
+
+        when(loanTypeRepository.existsById(VALID_LOAN_TYPE_ID)).thenReturn(Mono.just(true));
+        when(statusRepository.existsById(PENDING_STATUS_ID)).thenReturn(Mono.just(true));
+        when(applicationRepository.saveApplication(any(Application.class)))
+                .thenReturn(Mono.just(application.toBuilder().id(1L).build()));
+
+        Mono<Application> result = registerLoanApplicationUseCase.saveApplication(application);
+
+        StepVerifier.create(result)
+                .expectNextMatches(saved -> saved.getId().equals(1L))
+                .verifyComplete();
+    }
+
+    @Test
+    void shouldFailWhenLoanTypeDoesNotExist() {
+        Application application = Application.builder()
+                .amount(APPLICATION_AMOUNT)
+                .loanTypeId(INVALID_LOAN_TYPE_ID)
+                .statusId(PENDING_STATUS_ID)
+                .build();
+
+        when(loanTypeRepository.existsById(INVALID_LOAN_TYPE_ID)).thenReturn(Mono.just(false));
+        when(statusRepository.existsById(PENDING_STATUS_ID)).thenReturn(Mono.just(true));
+        Mono<Application> result = registerLoanApplicationUseCase.saveApplication(application);
+
+        StepVerifier.create(result)
+                .expectErrorMatches(throwable ->
+                        throwable instanceof BusinessException &&
+                                ((BusinessException) throwable).getErrors().containsKey("loanTypeId")
+                ).verify();
+    }
+
+    @Test
+    void shouldFailWhenStatusDoesNotExist() {
+        Application application = Application.builder()
+                .amount(APPLICATION_AMOUNT)
+                .loanTypeId(VALID_LOAN_TYPE_ID)
+                .statusId(PENDING_STATUS_ID)
+                .build();
+
+        when(loanTypeRepository.existsById(VALID_LOAN_TYPE_ID)).thenReturn(Mono.just(true));
+        when(statusRepository.existsById(PENDING_STATUS_ID)).thenReturn(Mono.just(false));
+
+        Mono<Application> result = registerLoanApplicationUseCase.saveApplication(application);
+
+        StepVerifier.create(result)
+                .expectErrorMatches(throwable ->
+                        throwable instanceof BusinessException &&
+                                ((BusinessException) throwable).getErrors().containsKey("statusId")
+                ).verify();
+    }
+
+    @Test
+    void shouldFailWhenLoanTypeAndStatusDoNotExist() {
+        Application application = Application.builder()
+                .amount(APPLICATION_AMOUNT)
+                .loanTypeId(INVALID_LOAN_TYPE_ID)
+                .statusId(PENDING_STATUS_ID)
+                .build();
+
+        when(loanTypeRepository.existsById(INVALID_LOAN_TYPE_ID)).thenReturn(Mono.just(false));
+        when(statusRepository.existsById(PENDING_STATUS_ID)).thenReturn(Mono.just(false));
+
+        Mono<Application> result = registerLoanApplicationUseCase.saveApplication(application);
+
+        StepVerifier.create(result)
+                .expectErrorMatches(throwable ->
+                        throwable instanceof BusinessException &&
+                                ((BusinessException) throwable).getErrors().containsKey("statusId") &&
+                                ((BusinessException) throwable).getErrors().containsKey("loanTypeId")
+                ).verify();
+    }
+
+}
